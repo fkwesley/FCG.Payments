4a97bb3dcada4157c2fd0515cd988c3c70e1bf68
fkwesley <frank-wesley95@hotmail.com>
2025-12-13 20:58:14 -0300

    Reapply "-migração AKS"
    This reverts commit b2c3bc199cddb361b42bb33936a0bec68df04ca3.

---
+++ Adapters/Driver/API/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// returns the current health status of the api.
+        /// </summary>
+        /// <returns>No content</returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+        [HttpGet(Name = "Health")]
+        public IActionResult Health()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
